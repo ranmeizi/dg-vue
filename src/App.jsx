95d18f939f484b938be3005bf7e1a3abc8e4ef1d
boboan <360969885@qq.com>
2020-03-29 19:34:11 +0800

    项目搭建

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from '@/routes'
+
+const router = new VueRouter({
+  routes
+})
+Vue.use(VueRouter)
+
+export default {
+  name: 'App',
+  router,
+  render(h) {
+    return <div id="app">
+      <router-view></router-view>
+    </div>
+  }
+}
